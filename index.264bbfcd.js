8c4a25fac34e5cae318d98492dc858570a15b4b2
Ivan Lavrinenko <lavrinenkoivan51@gmail.com>
2024-06-30 00:50:07 +0200

    make build

---
+++ index.264bbfcd.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".wall"),t=document.querySelector(".spider"),n=e.getBoundingClientRect(),o=n.width/2,c=n.height/2,d=t.getBoundingClientRect(),i=d.width,l=d.height;t.style.left="".concat(o-i/2,"px"),t.style.top="".concat(c-l/2,"px")});
+//# sourceMappingURL=index.264bbfcd.js.map
